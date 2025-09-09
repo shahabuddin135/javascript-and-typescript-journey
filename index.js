d34d38b9ccf5640bc873ed4e764258fcb83fc4ed
shahabuddin135 <voyagersvrs135@gmail.com>
2025-09-10 00:11:32 +0500

    öur first learning of variables

---
+++ index.js
@@ -0,0 +1,36 @@
+// var // you can change & also reassign the variable. (mutable)
+// let , you can change the value of the variable (mutable)
+// const // constant , you cannot change a variable value with const keyword (immutable)
+
+
+let fruit = "apple"
+
+// let fruit = "orange"
+
+fruit = "banana" // reassinging a value
+
+//variables are containers, which stores some data.
+
+
+// console.table(fruit)
+
+
+//folder is also called a directory
+
+
+// var toy = "car"
+
+// let toy = "dinasour"
+
+// var toy = "car"
+
+const car = "pink jeep"
+
+// car = "black jeeb" // not allowed
+
+console.log(car);
+
+Number 
+String
+Boolean
+
