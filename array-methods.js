666e9425ed4c3bf19b7817964aeb86021b9ebea6
shahabuddin135 <voyagersvrs135@gmail.com>
2025-09-13 01:08:31 +0500

    array methods and functions

---
+++ array-methods.js
@@ -0,0 +1,68 @@
+
+// let simple_array; //kabab case or snake case
+// 
+// let SimpleArray // Title case
+// 
+// let simpleArray // camel case
+// 
+// let SIMPLE_ARRAY // kabab case
+// 
+// let simple // lower case
+// 
+// let SIMPLE // upper case
+
+// upper case
+// lower case
+// title case
+// kabab case
+// camel case
+
+
+// let my-name (not allowed)
+
+//  let my_name (allowed)
+
+// let 1myName (not allowed)
+
+// let person1 (allowed)
+
+// let _car (allowed)
+
+// let $car (allowed)
+
+
+
+
+let numberArray = [9,2,7,6,5,4,3,8,1,0,10]
+
+// console.log( numberArray.reverse());
+
+// console.log(  numberArray.sort())
+
+// let a  = 2
+// let b = 3
+
+// console.log(a - b); // 2 - 3 = -1 //hence proved that a is smaller value
+
+
+
+// map => []
+// filter []
+// reduce => single value
+
+//  func signature
+// function sum () { function body OR block OR local Scope }
+
+function sum (num1,num2){
+   console.log(num1 + num2) // value printer
+}
+
+// sum(3,4)
+
+console.log(sum(3,7)); //undefined and prints 10 because it runs console.log
+
+function sub (num1,num2){
+   return num1 - num2 // value returned
+}
+
+console.log(sub(3,5)); // -2 
