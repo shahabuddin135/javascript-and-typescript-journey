a50b7d04dcb390b6e66f78a5ca2ecb94c56c8957
shahabuddin135 <voyagersvrs135@gmail.com>
2025-09-23 00:24:31 +0500

    objects and methods

---
+++ objects.js
@@ -0,0 +1,49 @@
+
+
+let hobbies = ["a","b"]
+
+let person = {
+    name:"Shahabuddin",
+    age:22,
+    gender:"male",
+    isMarried:false,
+    hobbies:["coding","scratch model crafts","gardening","astronomy"],
+    adress:{
+        country:"Pakistan",
+        city:"karachi",
+        ports:["karachi port","port qasim"],
+        isCleanCity:false,
+        skyscappers:5
+    },
+
+    showPersonData(){
+
+        console.log(this.name);
+        console.log(this.age);
+        console.log(this.adress.country);
+        console.log(this.adress.city);
+        
+    },
+
+    showHoobies: function(){
+       return this.hobbies  
+    },
+
+    sum(num1,num2){
+
+        return num1 + num2
+    }
+}
+
+person.showPersonData()
+
+console.log(person.showHoobies());
+
+let sum = person.sum(3,4)
+
+console.log(sum);
+
+
+
+
+console.log(person["name"]);
