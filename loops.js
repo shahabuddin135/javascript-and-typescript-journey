a3e7ac27cd4802b83ccf24da5d2950e0a62d3fa8
shahabuddin135 <voyagersvrs135@gmail.com>
2025-09-26 00:28:15 +0500

    for in and for off loops

---
+++ loops.js
@@ -0,0 +1,52 @@
+
+for( let count = 1; count <= 11; count ++ ){
+
+    console.log(count);
+    
+}
+
+
+const fruits = ["apple","banana","watermelon"]
+
+//for of loops
+for (let fruit of fruits){
+
+    console.log(`I love ${fruit}`);
+    
+}
+
+//for in loop
+for (let fruit in fruits){
+    console.log(fruit);
+    
+}
+
+
+//Comparision operators
+// < less than // 10 < 5 false // 5< 10 true
+// > more than // 1> 2 false // 3 > 1 true
+//  <= less than equal to 
+//  >= more than equal to 
+//  == double equal // 1 == "1" True
+//  === triple equal // 1 === "1" False
+
+
+// console.log(10 >= 5); // 
+
+//arthmetic operators 
+// 
+//+
+//  -
+//  /
+//  * 
+// ** exponential
+// % modulus
+
+
+//logical operators
+// &&  ,  ||, ! 
+
+
+
+// AND // 0 | 1 = 0
+// OR 1 | 0 = 1
--- mrf.js
+++ mrf.js
@@ -53,5 +53,4 @@ let numbers = [1,2,3,4,5];
 
 let double = numbers.map(number => number * 2)
 
-
 console.log(double);
